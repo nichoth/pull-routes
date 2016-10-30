af7630f29f460862b0285637976a6df2ed060906
Nick Thomas <nichoth@gmail.com>
2016-10-30 12:50:10 -0700

    do it simpler

--- index.js
+++ index.js
@@ -10,33 +10,19 @@ module.exports = function Start (routeEv) {
         var router = Router()
 
         // match url strings, subscribe to events, return a view
-        var view
         Object.keys(routes).forEach(function (path) {
-            router.addRoute(path, function onRoute (params) {
-                if (view) {
-                    view.source.end()
-                    view.sink.abort()
-                }
-                var streams = routes[path](params)
-                S( streams[0], streams[1], streams[2], streams[0] )
-                view = streams[0]
-                return streams[0].view
-            })
+            router.addRoute(path, routes[path])
         })
-
-        // map paths to views using `routes`
-        function onRoute (path) {
-            var m = router.match(path)
-            var view = m.fn(m.params, m)
-            return view
-        }
 
         // listen to route changes in browsers, return pushable that
         // takes route strings
         var routeStream = pushable()
         var stream = S(
             routeStream,
-            S.map(onRoute)
+            S.map(function (path) {
+                var m = router.match(path)
+                return m
+            })
         )
         stream.push = routeStream.push.bind(routeStream)
         if (routeEv && typeof document !== 'undefined') {
--- test/index.js
+++ test/index.js
@@ -1,98 +1,127 @@
 var test = require('tape')
 var S = require('pull-stream')
-var Pushable = require('pull-pushable')
-var Abortable = require('pull-abortable')
+// var Pushable = require('pull-pushable')
+// var Abortable = require('pull-abortable')
 var Router = require('../')
 
-function View (drain) {
-    var p = Pushable()
-    var abortable = Abortable()
-    var sink = S( abortable, drain )
-    sink.abort = abortable.abort.bind(abortable)
-    return {
-        source: p,
-        sink: sink,
-        view: 'test'
-    }
-}
-
-function Controller () {
-    return S.through()
-}
-
-function Store () {
-    return S.through()
-}
-
-test('handle routes', function (t) {
-    var routes = {
-        '/': function rootRoute (params) {
-            return [
-                View(S.log()),
-                Controller(),
-                Store()
-            ]
-        }
-    }
-
-    t.plan(1)
-    var router = Router()
-    var viewStream = router(routes)
-    S(
-        viewStream,
-        S.drain(function onEvent (view) {
-            t.equal(view, 'test', 'should stream .view property')
-        })
-    )
-    viewStream.push('/')
-})
-
-test('unsubscribe previous route', function (t) {
+test('map paths to routes', function (t) {
     t.plan(2)
-
-    var viewSource = Pushable(function onEnd (err) {
-        t.pass('should call view.source.end')
-    })
-
-    var viewSink = S.drain()
-    viewSink.abort = function () {
-        t.pass('should call view.sink.abort')
-    }
-
-    var routes = {
-        '/': function rootRoute (params) {
-            return [
-                {
-                    source: viewSource,
-                    sink: viewSink,
-                    view: '/'
-                },
-                Controller(),
-                Store()
-            ]
+    var rs = {
+        '/': function () {
+            return '/'
         },
-        '/test': function rootRoute (params) {
-            return [
-                {
-                    source: viewSource,
-                    sink: viewSink,
-                    view: '/test'
-                },
-                Controller(),
-                Store()
-            ]
+        '/one': function () {
+            return '/one'
         }
     }
 
     var router = Router()
-    var viewStream = router(routes)
+    var routeStream = router(rs)
+    var expected = [
+        '/',
+        '/one'
+    ]
+    var i = 0
     S(
-        viewStream,
-        S.drain(function onEvent (view) {
-            // console.log('drain', view)
+        routeStream,
+        S.drain(function onEvent (r) {
+            t.equal(r.fn(), expected[i++])
         })
-
     )
-    viewStream.push('/')
-    viewStream.push('/test')
+
+    routeStream.push('/')
+    routeStream.push('/one')
 })
+
+// function View (drain) {
+//     var p = Pushable()
+//     var abortable = Abortable()
+//     var sink = S( abortable, drain )
+//     sink.abort = abortable.abort.bind(abortable)
+//     return {
+//         source: p,
+//         sink: sink,
+//         view: 'test'
+//     }
+// }
+
+// function Controller () {
+//     return S.through()
+// }
+
+// function Store () {
+//     return S.through()
+// }
+
+// test('handle routes', function (t) {
+//     var routes = {
+//         '/': function rootRoute (params) {
+//             return [
+//                 View(S.log()),
+//                 Controller(),
+//                 Store()
+//             ]
+//         }
+//     }
+
+//     t.plan(1)
+//     var router = Router()
+//     var viewStream = router(routes)
+//     S(
+//         viewStream,
+//         S.drain(function onEvent (view) {
+//             t.equal(view, 'test', 'should stream .view property')
+//         })
+//     )
+//     viewStream.push('/')
+// })
+
+// test('unsubscribe previous route', function (t) {
+//     t.plan(2)
+
+//     var viewSource = Pushable(function onEnd (err) {
+//         t.pass('should call view.source.end')
+//     })
+
+//     var viewSink = S.drain()
+//     viewSink.abort = function () {
+//         t.pass('should call view.sink.abort')
+//     }
+
+//     var routes = {
+//         '/': function rootRoute (params) {
+//             return [
+//                 {
+//                     source: viewSource,
+//                     sink: viewSink,
+//                     view: '/'
+//                 },
+//                 Controller(),
+//                 Store()
+//             ]
+//         },
+//         '/test': function rootRoute (params) {
+//             return [
+//                 {
+//                     source: viewSource,
+//                     sink: viewSink,
+//                     view: '/test'
+//                 },
+//                 Controller(),
+//                 Store()
+//             ]
+//         }
+//     }
+
+//     var router = Router()
+//     var viewStream = router(routes)
+//     S(
+//         viewStream,
+//         S.drain(function onEvent (view) {
+//             // console.log('drain', view)
+//         })
+
+//     )
+//     viewStream.push('/')
+//     viewStream.push('/test')
+// })
